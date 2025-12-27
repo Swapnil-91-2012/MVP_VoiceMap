ab9de082504951e52402bfbe68bace9707fcb3d5
Swapnil Bag <swapnilbag91@gmail.com>
2025-12-27 18:51:20 +0530

    Update app.py

--- MVP_VoiceMap/backend/app.py
+++ MVP_VoiceMap/backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, send_from_directory
+from flask_cors import CORS
 import os
 import sys
 
 app = Flask(__name__)
+CORS(app)  
 
 # ---------------- HEALTH ---------------- #
 
